21b788871ee4da157f647ecd4e362ec213470e65
Anton Timchenko <anton.timchenko@outlook.com>
2020-09-08 16:11:22 +0300

    Added support for different api methods

--- src/main/kotlin/me/antandtim/telegram/api/domain/TelegramConnection.kt
+++ src/main/kotlin/me/antandtim/telegram/api/domain/TelegramConnection.kt
@@ -1,16 +1,17 @@
 package me.antandtim.telegram.api.domain
 
-import org.telegram.telegrambots.meta.api.methods.send.SendMessage
-import org.telegram.telegrambots.meta.api.objects.Update
 import me.antandtim.telegram.api.service.UserStepService
+import org.telegram.telegrambots.meta.api.methods.BotApiMethod
+import org.telegram.telegrambots.meta.api.objects.Update
+import java.io.Serializable
 
-data class TelegramConnection(
-  val update: Update,
-  val sendMessage: (SendMessage) -> Unit,
-  val step: String = "",
-  private val userStepService: UserStepService
+data class TelegramConnection<T : BotApiMethod<out Serializable>, R : Any?>(
+    val update: Update,
+    val send: (T) -> R,
+    val step: String = "",
+    private val userStepService: UserStepService
 ) {
-  fun setStep(step: String) {
-    userStepService.setStep(update.message.chatId, step)
-  }
+    fun setStep(step: String) {
+        userStepService.setStep(update.message.chatId, step)
+    }
 }
